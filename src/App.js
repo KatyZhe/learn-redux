ee721b29fdcb4651001072a8200bc551c0c22fc7
Katy_Zhe <nekhaeva.e@yandex.ru>
2024-06-13 21:05:00 +0300

    13.06.24

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
+
+function App() {
+  const dispatch = useDispatch();
+  const cash = useSelector((state) => state.cash);
+
+  const addCash = (cash) => {
+    dispatch({ type: "ADD_CASH", payload: cash });
+  };
+
+  const getCash = (cash) => {
+    dispatch({ type: "GET_CASH", payload: cash });
+  };
+
+  return (
+    <body>
+      <div class="container">
+        <div class="number">{cash}</div>
+        <div class="buttons">
+          <button class="button" onClick={() => addCash(Number(prompt()))}>
+            Пополнить счет
+          </button>
+          <button class="button" onClick={() => getCash(Number(prompt()))}>
+            Снять со счета
+          </button>
+        </div>
+      </div>
+    </body>
+  );
+}
+
+export default App;
